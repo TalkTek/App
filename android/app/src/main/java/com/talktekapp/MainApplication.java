d6d07e4247a499e9ae0db4011d2522d00ec502a8
Rae Hsieh <joey3060@gmail.com>
2017-05-19 14:58:23 +0800

    facebook login android env done

--- android/app/src/main/java/com/talktekapp/MainApplication.java
+++ android/app/src/main/java/com/talktekapp/MainApplication.java
@@ -12,11 +12,20 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.futurice.rctaudiotoolkit.AudioPackage;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
 
@@ -33,12 +42,12 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new FBSDKPackage(),
-            new CodePush("Fa0Ug7tNp1xFH9EShk5GXlBEIXCE4JbRhcGg7", getApplicationContext(), BuildConfig.DEBUG),
-            new VectorIconsPackage(),
-            new RNSoundPackage(),
-            new AudioPackage()
+              new MainReactPackage(),
+              new CodePush("Fa0Ug7tNp1xFH9EShk5GXlBEIXCE4JbRhcGg7", getApplicationContext(), BuildConfig.DEBUG),
+              new VectorIconsPackage(),
+              new RNSoundPackage(),
+              new AudioPackage(),
+              new FBSDKPackage(mCallbackManager)
       );
     }
   };
@@ -51,6 +60,8 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+    FacebookSdk.sdkInitialize(getApplicationContext());
+    // If you want to use AppEventsLogger to log events.
+    AppEventsLogger.activateApp(this);
   }
 }
