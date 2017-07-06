909af13520a92bd31ebfc6ee4e644dfcb0284d0c
Rae Hsieh <joey3060@gmail.com>
2017-07-06 12:52:44 +0800

    PlayAudioScreen Layout done

--- src/screens/playAudio/index.js
+++ src/screens/playAudio/index.js
@@ -19,39 +19,61 @@ import {
   Image
 } from 'react-native'
 import styles from './styles'
+import Slider from 'react-native-slider'
 
 
 const buttons = {
   close: require('../../assets/img/playAudio/close.png'),
   footer: {
-    good: {
-      link: require('../../assets/img/playAudio/good.png'),
-      name: '203'
+    active: {
+      goodActive: {
+        link: require('../../assets/img/playAudio/goodActive.png'),
+        name: '203'
+      },
+      timerActive: {
+        link: require('../../assets/img/playAudio/timerActive.png'),
+        name: '20:39'
+      },
     },
-    goodActive: {
-      link: require('../../assets/img/playAudio/goodActive.png'),
-      name: '203'
-    },
-    timer: {
-      link: require('../../assets/img/playAudio/timer.png'),
-      name: '20:39'
-    },
-    timerActive: {
-      link: require('../../assets/img/playAudio/timerActive.png'),
-      name: '20:39'
-    },
-    addSpeed: {
-      link: require('../../assets/img/playAudio/addSpeed.png'),
-      name: '速率'
-    },
-    word: {
-      link: require('../../assets/img/playAudio/word.png'),
-      name: '文檔'
-    },
-    more: {
-      link: require('../../assets/img/playAudio/more.png'),
-      name: '更多'
+    notActive: {
+      good: {
+        link: require('../../assets/img/playAudio/good.png'),
+        name: '203'
+      },
+      timer: {
+        link: require('../../assets/img/playAudio/timer.png'),
+        name: '20:39'
+      },
+      addSpeed: {
+        link: require('../../assets/img/playAudio/addSpeed.png'),
+        name: '速率'
+      },
+      word: {
+        link: require('../../assets/img/playAudio/word.png'),
+        name: '文檔'
+      },
+      more: {
+        link: require('../../assets/img/playAudio/more.png'),
+        name: '更多'
+      }
     }
+  },
+  body: {
+    backward15: {
+      link: require('../../assets/img/audioElement/backward15.png')
+    },
+    backward: {
+      link: require('../../assets/img/audioElement/backward.png')
+    },
+    play: {
+      link: require('../../assets/img/playAudio/play.png')
+    },
+    forward: {
+      link: require('../../assets/img/audioElement/forward.png')
+    },
+    forward15: {
+      link: require('../../assets/img/audioElement/forward15.png')
+    },
   }
 }
 
@@ -62,24 +84,41 @@ export default class PlayAudio extends Component {
   }
   render () {
     const { goBack } = this.props.navigation
-    const funcButtons = Object.values(buttons.footer).map((button, i) => (
-      <Button
-        transparent
+    const footerButtons = Object.values(buttons.footer.notActive).map((button, i) => {
+      return (
+        <TouchableHighlight
+          transparent
+          key={i}
+        >
+          <View style={styles.footerFunUnit}>
+            <Image
+              source={button.link}
+              style={styles.footerImages}
+            />
+            <Text
+              style={styles.footerText}
+            >
+              {button.name}
+            </Text>
+          </View>
+        </TouchableHighlight>
+      )
+    })
+
+    const bodyButtons = Object.values(buttons.body).map((button, i) => (
+      <TouchableHighlight
+        key={i}
       >
-        <View>
-          <Image
-            source={button.link}
-          />
-          <Text>
-            {button.name}
-          </Text>
-        </View>
-      </Button>
+        <Image
+          source={button.link}
+          style={styles.bodyImages}
+        />
+      </TouchableHighlight>
     ))
 
     return (
-      <Container>
-        <Header>
+      <Container style={styles.container}>
+        <Header style={styles.header}>
           <Left/>
           <Body/>
           <Right>
@@ -99,10 +138,38 @@ export default class PlayAudio extends Component {
              source={require('../../assets/img/knowledgeCapsule/banner.png')}
            />
           </View>
+          <View style={styles.body}>
+            <View style={styles.title}>
+              <Text style={styles.titleText}>
+                別期待你的男人一心多用 
+              </Text>
+            </View>
+            <View style={styles.audioType}>
+              <Text style={styles.audioTypeText}>
+                #方法技能
+              </Text>
+            </View>
+            <View style={styles.slider}>
+              <View style={styles.sliderTime}>
+                <Text style={styles.sliderTimeText}>00:48</Text>
+                <Text/>
+                <Text style={styles.sliderTimeText}>02:56</Text>
+              </View>
+              <Slider
+                minimumTrackTintColor='rgb(31, 191, 179)'
+                thumbTouchSize={{width: 20, height: 20}}
+                trackStyle={styles.track}
+                thumbStyle={styles.trackThumb}
+              />
+            </View>
+            <View style={styles.audioFunc}>
+              {bodyButtons}
+            </View>
+          </View>
         </Content>
-        <Footer>
-          {funcButtons}
-        </Footer>
+        <View style={styles.footer}>
+          {footerButtons}
+        </View>
       </Container>
     )
   }
--- src/screens/playAudio/styles.js
+++ src/screens/playAudio/styles.js
@@ -9,14 +9,91 @@ export default {
   container: {
     backgroundColor: 'white'
   },
+  header: {
+    backgroundColor: 'white',
+    borderBottomWidth: 0
+  },
   banner: {
     flex: 1,
     height: screenHeight * 0.34,
     backgroundColor: 'white',
     justifyContent: 'center'
   },
+  body: {
+    height: 290,
+    paddingHorizontal: screenWidth * 0.085,
+    paddingTop: 24,
+    alignItems: 'center'
+  },
+  title: {
+    marginBottom: 16
+  },
+  titleText: {
+    fontSize: 18,
+    lineHeight: 24,
+    color: 'rgb(33 ,33 ,33)'
+  },
+  audioType: {
+    marginBottom: 32
+  },
+  slider: {
+    width: screenWidth * 0.83,
+    marginBottom: 10
+  },
+  track: {
+    height: 2,
+    borderRadius: 2,
+    backgroundColor: 'rgb(238, 238, 238)'
+  },
+  trackThumb: {
+    top: 22,
+    width: 20,
+    height: 20,
+    borderRadius: 30 / 2,
+    backgroundColor: 'white',
+    borderColor: 'rgb(31, 191, 179)',
+    borderWidth: 2
+  },
+  sliderTime: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  sliderTimeText: {
+    fontSize: 9,
+    color: 'rgb(158, 158, 158)'
+  },
+  audioTypeText: {
+    fontSize: 13,
+    lineHeight: 13,
+    color: 'rgb(158 ,158, 158)'
+  },
+  audioFunc: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: screenWidth * 0.915
+  },
   footer: {
     flex: 1,
-    flexDirection: 'row'
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: 10,
+    borderTopWidth: 1,
+    borderTopColor: 'rgb(224, 224, 224)',
+    height: 75,
+    width: screenWidth,
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  footerFunUnit: {
+    alignItems: 'center'
+  },
+  footerImages: {
+    width: 40,
+    height: 40
+  },
+  footerText: {
+    fontSize: 10,
+    color: 'rgb(189, 189, 189)'
   }
 }
