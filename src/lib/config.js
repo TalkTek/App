5fb93326f743d8f13eb983fa80fe1ec977ab2a1a
Rae Hsieh <joey3060@gmail.com>
2017-07-24 10:30:12 +0800

    add the production, stage, and dev firebase

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,8 +1,26 @@
-export const FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyDg8EtzEdylETw4NkraElAhC4uEDw-eimA',
-  authDomain: 'talktek-4edac.firebaseapp.com',
-  databaseURL: 'https://talktek-4edac.firebaseio.com',
-  projectId: 'talktek-4edac',
-  storageBucket: 'talktek-4edac.appspot.com',
-  messagingSenderId: '1039536580500'
+export default {
+  PRODUCTION: {
+    apiKey: 'AIzaSyALzKdQfhk7kdM5-C5dAMcecIkK57Qenyw',
+    authDomain: 'talktekproduction.firebaseapp.com',
+    databaseURL: 'https://talktekproduction.firebaseio.com',
+    projectId: 'talktekproduction',
+    storageBucket: '',
+    messagingSenderId: '483803817924'
+  },
+  STAGE: {
+    apiKey: 'AIzaSyCMyLws4mra3HZbC8GKHCQyaZfupRt2VTs',
+    authDomain: 'talktekstage.firebaseapp.com',
+    databaseURL: 'https://talktekstage.firebaseio.com',
+    projectId: 'talktekstage',
+    storageBucket: '',
+    messagingSenderId: '375120402670'
+  },
+  DEV: {
+    apiKey: 'AIzaSyDg8EtzEdylETw4NkraElAhC4uEDw-eimA',
+    authDomain: 'talktek-4edac.firebaseapp.com',
+    databaseURL: 'https://talktek-4edac.firebaseio.com',
+    projectId: 'talktek-4edac',
+    storageBucket: 'talktek-4edac.appspot.com',
+    messagingSenderId: '1039536580500'
+  }
 }
