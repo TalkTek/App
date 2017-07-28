b101baf9fddada45b070326a43fd39ed4b33614d
菜鳥祐 <a9650615@gmail.com>
2017-07-28 16:45:46 +0800

    connect to google analytic

--- src/reducer/analytic/analyticSaga.js
+++ src/reducer/analytic/analyticSaga.js
@@ -3,17 +3,20 @@ import {
   takeEvery
 } from 'redux-saga/effects'
 import analyticTypes from './analyticTypes'
+import Analytic from '../../api/Analytic'
 
 /**
  * subroutines
  */
 
-function * setScreen (screen) {
-  console.log(screen)
+function * setScreen ({ payload }) {
+  console.log(payload)
+  new Analytic().setScreen(payload)
 }
 
 function * setEvent ({ payload: { category, action, value } }) {
   console.log(category, action, value)
+  new Analytic().track(category, action, value)
 }
 
 /**
