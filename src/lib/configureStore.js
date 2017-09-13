de8ccf06e4ebff5eb74a3dccc9f949bb15353a8f
2sx <joey3060@gmail.com>
2017-09-13 10:55:44 +0800

    remove unused packages and codes

--- src/lib/configureStore.js
+++ src/lib/configureStore.js
@@ -1,12 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux'
-// import { persistStore, autoRehydrate } from 'redux-persist';
 import { AsyncStorage } from 'react-native';
 import middlewares, { sagaMiddlewares } from './middlewares'
 import reducer from '../reducer/index'
 import configSaga from './configSagas'
 
 const enhancers = [
-  // autoRehydrate(),
   applyMiddleware(...middlewares)
 ]
 
@@ -17,10 +15,6 @@ const enhancer = composeEnhancers(...enhancers)
 function configureStore(onComplete: ?()=> void) {
   const store = createStore(reducer, undefined, enhancer)
   sagaMiddlewares.run(configSaga)
-  // persistStore(store, {storage: AsyncStorage}, onComplete)
-  // if(__DEV__) {
-  //   window.store = store
-  // }
   return store
 }
 
